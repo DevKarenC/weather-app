465466219ba5cf71c664022a52e66a30351b7660
Karen <devkarenc@gmail.com>
2021-03-11 22:57:42 -0500

    Set up webpack, eslint, and prettier configuration files

---
+++ .babelrc.js
@@ -0,0 +1,9 @@
+module.exports = function (api) {
+  api.cache(true);
+  const presets = [
+    ['@babel/preset-env', { useBuiltIns: 'usage', corejs: '3.6' }],
+  ];
+  return {
+    presets,
+  };
+};
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    }),
+    new HtmlWebpackPlugin({
+      title: "What's the Weather?",
+      template: 'src/index.html',
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+};
